6cea4ca423b680723b8337edeeb16c720591b7d1
Rakshasss <gchanturidze24@gmail.com>
2023-08-22 16:35:26 +0400

    minor fixes of <hr> and imges

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const slider = document.querySelector('.slider');
+/* const slider = document.querySelector('.slider');
 const images = slider.querySelectorAll('img');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
@@ -43,6 +43,35 @@ slider.addEventListener('mouseenter', () => {
 slider.addEventListener('mouseleave', () => {
     slideInterval = setInterval(goToNextSlide, 5000);
 });
+ */
 
 
 
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+let currentSlideIndex = 0;
+
+prevButton.addEventListener('click', () => {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    updateSlider();
+});
+
+nextButton.addEventListener('click', () => {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    updateSlider();
+});
+
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === currentSlideIndex) {
+            slide.style.transform = 'translateX(0)';
+        } else if (index < currentSlideIndex) {
+            slide.style.transform = 'translateX(-100%)';
+        } else {
+            slide.style.transform = 'translateX(100%)';
+        }
+    });
+}
+
+updateSlider();
