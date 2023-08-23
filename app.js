206c02cc4ea78a0760f759a79c8b96f1b7e63290
Rakshasss <gchanturidze24@gmail.com>
2023-08-24 02:14:28 +0400

    visual fixes: bars, margins cota responsive da latest projects foundation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* const slider = document.querySelector('.slider');
+const slider = document.querySelector('.slider');
 const images = slider.querySelectorAll('img');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
@@ -32,46 +32,4 @@ for (let i = 1; i < images.length; i++) {
     images[i].classList.add('hidden-image');
 }
 
-
-const slideInterval = setInterval(goToNextSlide, 5000);
-
-
-slider.addEventListener('mouseenter', () => {
-    clearInterval(slideInterval);
-});
-
-slider.addEventListener('mouseleave', () => {
-    slideInterval = setInterval(goToNextSlide, 5000);
-});
- */
-
-
-
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
-let currentSlideIndex = 0;
-
-prevButton.addEventListener('click', () => {
-    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-    updateSlider();
-});
-
-nextButton.addEventListener('click', () => {
-    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-    updateSlider();
-});
-
-function updateSlider() {
-    slides.forEach((slide, index) => {
-        if (index === currentSlideIndex) {
-            slide.style.transform = 'translateX(0)';
-        } else if (index < currentSlideIndex) {
-            slide.style.transform = 'translateX(-100%)';
-        } else {
-            slide.style.transform = 'translateX(100%)';
-        }
-    });
-}
-
-updateSlider();
+const slideInterval = setInterval(goToNextSlide, 1000);
