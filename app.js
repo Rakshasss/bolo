7f298c1ae5831d77aafb014bd5988bdce8c6c09d
Rakshasss <gchanturidze24@gmail.com>
2023-08-27 03:41:13 +0400

    js for div slider footer with inputs and little fixes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const slider = document.querySelector('.slider');
+const slider = document.querySelector('.slider-container');
 const images = slider.querySelectorAll('img');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 let currentSlide = 0;
-
 
 function goToNextSlide() {
     hideCurrentSlide();
@@ -27,22 +26,82 @@ function revealCurrentSlide() {
     images[currentSlide].classList.remove('hidden-image');
 }
 
-
 for (let i = 1; i < images.length; i++) {
     images[i].classList.add('hidden-image');
 }
-
-const slideInterval = setInterval(goToNextSlide, 1000);
-let slideIndex = 0;
-let interval;
-function showSlide(index) { const slides = document.querySelectorAll('.slide'); if (index < 0) { slideIndex = slides.length - 1; } else if (index >= slides.length) { slideIndex = 0; } for (let i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; } slides[slideIndex].style.display = 'block'; }
-function changeSlide(n) { slideIndex += n; showSlide(slideIndex); }
-function autoSlide() { changeSlide(1); }
-showSlide(slideIndex);
-interval = setInterval(autoSlide, 5000);
-function isElementInViewport(element) { var rect = element.getBoundingClientRect(); return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth)); }
+setInterval(() => {
+    goToNextSlide();
+}, 2000);
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 const resizingDesign = document.getElementById('design');
 const resizingDevelopment = document.getElementById('development');
 const resizingProduction = document.getElementById('production');
 const resizingMarking = document.getElementById('marking');
-window.addEventListener('scroll', function () { if (isElementInViewport(resizingDesign)) { console.log('Div is visible!'); resizingDesign.style.width = '80%'; } }); // You can perform actions here when the div is visible } if (isElementInViewport(resizingDevelopment)) { console.log('Div is visible!'); resizingDevelopment.style.width = '40%'; // You can perform actions here when the div is visible } if (isElementInViewport(resizingMarking)) { console.log('Div is visible!'); resizingMarking.style.width = '90%'; // You can perform actions here when the div is visible } if (isElementInViewport(resizingProduction)) { console.log('Div is visible!'); resizingProduction.style.width = '25%'; // You can perform actions here when the div is visible } });
+
+window.addEventListener('scroll', function () {
+    if (isElementInViewport(resizingDesign)) {
+        console.log('Div is visible!');
+        resizingDesign.style.width = '80%';
+    }
+    if (isElementInViewport(resizingDevelopment)) {
+        console.log('Div is visible!');
+        resizingDevelopment.style.width = '40%';
+    }
+    if (isElementInViewport(resizingMarking)) {
+        console.log('Div is visible!');
+        resizingMarking.style.width = '90%';
+    }
+    if (isElementInViewport(resizingProduction)) {
+        console.log('Div is visible!');
+        resizingProduction.style.width = '25%';
+    }
+}); // You can perform actions here when the div is visible } if (isElementInViewport(resizingDevelopment)) { console.log('Div is visible!'); resizingDevelopment.style.width = '40%'; // You can perform actions here when the div is visible } if (isElementInViewport(resizingMarking)) { console.log('Div is visible!'); resizingMarking.style.width = '90%'; // You can perform actions here when the div is visible } if (isElementInViewport(resizingProduction)) { console.log('Div is visible!'); resizingProduction.style.width = '25%'; // You can perform actions here when the div is visible } });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.slide');
+    const slideButtons = document.querySelectorAll('.slideButton');
+
+    slideButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const slideId = button.getAttribute('data-slide-id');
+            console.log(slideId);
+            showSlideSecond(slideId);
+        });
+    });
+
+    function showSlideSecond(slideId) {
+        slides.forEach((slide) => slide.classList.remove('active'));
+        const slideToShow = document.getElementById(slideId);
+        slideToShow.classList.add('active');
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const hoverTexts = document.querySelectorAll('.hover-text');
+    const images = document.querySelectorAll('.image');
+
+    hoverTexts.forEach((text, index) => {
+        text.addEventListener('click', () => {
+            setActiveText(text);
+            setActiveImage(index);
+        });
+    });
+
+    function setActiveText(activeText) {
+        hoverTexts.forEach((text) => text.classList.remove('active'));
+        activeText.classList.add('active');
+    }
+
+    function setActiveImage(activeIndex) {
+        images.forEach((image) => image.classList.remove('active'));
+        images[activeIndex].classList.add('active');
+    }
+});
