9bace21c0e26486850ce09e63e334283782d796a
Rakshasss <gchanturidze24@gmail.com>
2023-08-28 02:13:27 +0400

    no bugs in js

--- app.js
+++ app.js
@@ -74,19 +74,35 @@ document.addEventListener('DOMContentLoaded', function () {
         button.addEventListener('click', () => {
             const slideId = button.getAttribute('data-slide-id');
             console.log(slideId);
-            showSlideSecond(slideId);
+            showSlideSecond(slideId, button);
         });
     });
 
-    function showSlideSecond(slideId) {
+    function showSlideSecond(slideId, button) {
         slides.forEach((slide) => slide.classList.remove('active'));
+        slideButtons.forEach((button) => button.classList.remove('active'));
         const slideToShow = document.getElementById(slideId);
         slideToShow.classList.add('active');
+        button.classList.add('active');
     }
 });
 document.addEventListener('DOMContentLoaded', function () {
     const hoverTexts = document.querySelectorAll('.hover-text');
-    const images = document.querySelectorAll('.image');
+    const images = document.querySelectorAll('.pimage');
+    const ALL = document.querySelectorAll(".yvela");
+    let all_toggle = false;
+
+    ALL.forEach((text) => {
+        text.addEventListener("click", () => {
+            if (all_toggle) {
+                deactivateAllText();
+                deactivateAllImg();
+            } else {
+                deactivateAllText();
+                setActiveAllImg();
+            }
+        })
+    });
 
     hoverTexts.forEach((text, index) => {
         text.addEventListener('click', () => {
@@ -95,13 +111,96 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    function setActiveText(activeText) {
+    function setActiveAllImg() {
+        [...images].filter(v => v.classList.contains("active") === false).forEach((image) => image.classList.add("active"));
+        all_toggle = true;
+    }
+
+    function deactivateAllImg() {
+        [...images].filter(v => v.classList.contains("active") === true).forEach((image) => image.classList.remove("active"));
+        all_toggle = false;
+    }
+
+    function deactivateAllText() {
         hoverTexts.forEach((text) => text.classList.remove('active'));
+    }
+
+    function setActiveText(activeText) {
+        if (activeText.classList.contains("active") && !all_toggle) {
+            deactivateAllText();
+            activeText.classList.remove("active");
+            return;
+        }
+        deactivateAllText();
         activeText.classList.add('active');
     }
 
     function setActiveImage(activeIndex) {
-        images.forEach((image) => image.classList.remove('active'));
+        if (images[activeIndex].classList.contains("active") && !all_toggle) {
+            deactivateAllImg();
+            images[activeIndex].classList.remove("active");
+            return;
+        }
+        deactivateAllImg();
         images[activeIndex].classList.add('active');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const nameInput = document.getElementById('nameInput');
+    const emailInput = document.getElementById('emailInput');
+    const webInput = document.getElementById('webInput');
+    const messageInput = document.getElementById('messageInput');
+    const submitButton = document.getElementById('submitButton');
+    const outputDiv = document.getElementById('output');
+
+    submitButton.addEventListener('click', function () {
+        const name = nameInput.value;
+        const email = emailInput.value;
+        const web = webInput.value;
+        const message = messageInput.value;
+        postData(name, email, web, message);
+    });
+
+    async function postData(name, email, web, message) {
+        if ((name == null || name == "") && (email == null || email == "") && (message == null || message == "")) {
+            alert("Name Email and Message must be FILLED IN");
+            console.log("rame")
+            return;
+        }
+        try {
+            const dataToSend = {
+                name: name,
+                email: email,
+                web: web,
+                message: message,
+            };
+
+            const response = await fetch(
+                'https://borjomi.loremipsum.ge/api/send-message', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataToSend),
+            }
+            );
+
+            const data = await response.json();
+            console.log(data);
+            displayResponse(data);
+        } catch (error) {
+            console.error(`The Error is: ${error}`);
+        }
+    }
+
+    function displayResponse(data) {
+        if (data.status === 1) {
+            outputDiv.innerHTML = `
+          <h2>Response:</h2>
+          <pre>${data.desc}</pre>
+      `;
+        }
     }
 });
